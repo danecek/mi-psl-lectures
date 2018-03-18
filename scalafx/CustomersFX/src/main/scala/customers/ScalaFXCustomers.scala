300950c55ffbf7b79a2e40fd32c0da5193243f7b
danecek <danecek@fit.cvut.cz>
2018-03-18 21:49:49 +0100

    m

--- scalafx/CustomersFX/src/main/scala/customers/ScalaFXCustomers.scala
+++ scalafx/CustomersFX/src/main/scala/customers/ScalaFXCustomers.scala
@@ -4,8 +4,6 @@ import customers.view.MainWindow
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.control.ScrollPane
-import scalafx.scene.layout.BorderPane
 
 
 object ScalaFXCustomers extends JFXApp {
@@ -13,8 +11,5 @@ object ScalaFXCustomers extends JFXApp {
   stage = new PrimaryStage {
     title = "ScalaFX Customers"
     scene = new Scene(MainWindow, 800, 600)
-//    {
-//      root = new MainWindow
-//    }
   }
 }
